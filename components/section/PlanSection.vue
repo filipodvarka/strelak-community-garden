c8b0275489d4e2e382741afa8e6cf6c063880355
Filip Odvarka <filipodvarka1@gmail.com>
2025-01-29 08:54:38 +0100

    Add 2024 poster

--- components/section/PlanSection.vue
+++ components/section/PlanSection.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="max-w-screen-lg mx-auto py-16 lg:py-20">
+  <div class="max-w-screen-lg mx-auto pt-12">
     <h2 class="font-barrio text-2xl lg:text-3xl text-primary-600 mb-4">
       Sezona 2024
     </h2>
 
     <picture>
-      <img
-        src="/poster_support_2024.jpg"
-        width="2480"
-        height="3508"
-        alt="záhon s několika květinami"
+      <nuxt-img
+        src="/plakat_2024.png"
+        width="1024"
+        height="1448"
+        alt="program 2024"
         class="w-full"
         loading="lazy"
-      >
+      />
     </picture>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <PlanSection />
     <InfoSection />
     <RulesSection />
     <ClientOnly>
